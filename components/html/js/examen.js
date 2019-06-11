48e71db682d8a1ea494e41abc9556c3b0e873ff8
PeibolRam <ramirezvazquezp@gmail.com>
2019-06-10 22:18:11 -0500

    examen

---
+++ components/html/js/examen.js
@@ -0,0 +1,257 @@
+function examinar() 
+{ 
+puntuacion=0; 
+nocontesta=0; 
+contestadas=0; 
+puntosmaximos=0; 
+
+if(pregunta1!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta1 == "A") {puntuacion=puntuacion+0}
+if(pregunta1 == "B") {puntuacion=puntuacion+0}
+if(pregunta1 == "C") {puntuacion=puntuacion+5}
+if(pregunta1 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta2 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta2 == "A") {puntuacion=puntuacion+0}
+if(pregunta2 == "B") {puntuacion=puntuacion+0}
+if(pregunta2 == "C") {puntuacion=puntuacion+5}
+if(pregunta2 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta3!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta3 == "A") {puntuacion=puntuacion+0}
+if(pregunta3 == "B") {puntuacion=puntuacion+0}
+if(pregunta3 == "C") {puntuacion=puntuacion+5}
+if(pregunta3 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta4 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta4 == "A") {puntuacion=puntuacion+0}
+if(pregunta4 == "B") {puntuacion=puntuacion+0}
+if(pregunta4 == "C") {puntuacion=puntuacion+5}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta5!= "0" ){
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta5 == "A") {puntuacion=puntuacion+5}
+if(pregunta5 == "B") {puntuacion=puntuacion+0}
+if(pregunta5 == "C") {puntuacion=puntuacion+0}
+if(pregunta5 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta6 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta6 == "A") {puntuacion=puntuacion+0}
+if(pregunta6 == "B") {puntuacion=puntuacion+0}
+if(pregunta6 == "C") {puntuacion=puntuacion+0}
+if(pregunta6 == "D") {puntuacion=puntuacion+5}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta7!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta7 == "A") {puntuacion=puntuacion+0}
+if(pregunta7 == "B") {puntuacion=puntuacion+5}
+if(pregunta7 == "C") {puntuacion=puntuacion+0}
+if(pregunta7 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta8 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta8 == "A") {puntuacion=puntuacion+0}
+if(pregunta8 == "B") {puntuacion=puntuacion+0}
+if(pregunta8 == "C") {puntuacion=puntuacion+0}
+if(pregunta8 == "D") {puntuacion=puntuacion+5}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta9!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta9 == "A") {puntuacion=puntuacion+0}
+if(pregunta9 == "B") {puntuacion=puntuacion+0}
+if(pregunta9 == "C") {puntuacion=puntuacion+0}
+if(pregunta9 == "D") {puntuacion=puntuacion+5}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta10 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta10 == "A") {puntuacion=puntuacion+5}
+if(pregunta10 == "B") {puntuacion=puntuacion+0}
+if(pregunta10 == "C") {puntuacion=puntuacion+0}
+if(pregunta10 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta11!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta11 == "A") {puntuacion=puntuacion+0}
+if(pregunta11 == "B") {puntuacion=puntuacion+5}
+if(pregunta11 == "C") {puntuacion=puntuacion+0}
+if(pregunta11 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+
+if(pregunta12 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta12 == "A") {puntuacion=puntuacion+5}
+if(pregunta12 == "B") {puntuacion=puntuacion+0}
+if(pregunta12 == "C") {puntuacion=puntuacion+0}
+if(pregunta12 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta13 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta13 == "A") {puntuacion=puntuacion+0}
+if(pregunta13 == "B") {puntuacion=puntuacion+0}
+if(pregunta13 == "C") {puntuacion=puntuacion+0}
+if(pregunta13 == "D") {puntuacion=puntuacion+5}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta14 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta14 == "A") {puntuacion=puntuacion+0}
+if(pregunta14 == "B") {puntuacion=puntuacion+0}
+if(pregunta14 == "C") {puntuacion=puntuacion+5}
+if(pregunta14 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta15 != "0"){
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta15 == "A") {puntuacion=puntuacion+5}
+if(pregunta15 == "B") {puntuacion=puntuacion+0}
+if(pregunta15 == "C") {puntuacion=puntuacion+0}
+if(pregunta15 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta16 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta16 == "A") {puntuacion=puntuacion+0}
+if(pregunta16 == "B") {puntuacion=puntuacion+0}
+if(pregunta16 == "C") {puntuacion=puntuacion+5}
+if(pregunta16 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta17!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta17 == "A") {puntuacion=puntuacion+0}
+if(pregunta17 == "B") {puntuacion=puntuacion+0}
+if(pregunta17 == "C") {puntuacion=puntuacion+5}
+if(pregunta17 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta18 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta18 == "A") {puntuacion=puntuacion+5}
+if(pregunta18 == "B") {puntuacion=puntuacion+0}
+if(pregunta18 == "C") {puntuacion=puntuacion+0}
+if(pregunta18 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if(pregunta19!= "0" )
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta19 == "A") {puntuacion=puntuacion+5}
+if(pregunta19 == "B") {puntuacion=puntuacion+0}
+if(pregunta19 == "C") {puntuacion=puntuacion+0}
+if(pregunta19 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1} 
+
+if(pregunta20 != "0")
+{
+contestadas=contestadas+1
+puntosmaximos=puntosmaximos+5
+if(pregunta20 == "A") {puntuacion=puntuacion+0}
+if(pregunta20 == "B") {puntuacion=puntuacion+0}
+if(pregunta20 == "C") {puntuacion=puntuacion+5}
+if(pregunta20 == "D") {puntuacion=puntuacion+0}
+}
+else {nocontesta=nocontesta+1}
+
+if (puntosmaximos!=0) 
+{indiceacierto=Math.round(100*(puntuacion/puntosmaximos))
+} 
+else{indiceacierto=0} 
+
+
+if(indiceacierto<=100&&indiceacierto>=90) 
+{
+mensaje="HAY WEY!!!!, SI SABES"} 
+
+if(indiceacierto<89&&indiceacierto>=75)
+{
+mensaje="PUES TE DEFIENDES XD"} 
+
+if(indiceacierto<74&&indiceacierto>=60)
+{
+mensaje="NO TAN BIEN PERO NO TAN MAL"} 
+
+if(indiceacierto<59&&indiceacierto>=30)
+{
+mensaje="PONTE A ESTUDIAR WEY"} 
+
+if(indiceacierto<29&&indiceacierto>=1)
+{
+mensaje="PROFE, REPRUEBELO!!!!"} 
+
+if(contestadas==0)
+{
+mensaje=":-|"} 
+
+document.getElementById("resultado").innerHTML = "Tu puntuación es "+puntuacion+"." +  "\n\nEl número máximo de puntos que podía conseguir era de " + puntosmaximos + ".\n\nHa dejado sin contestar "+ nocontesta+".\n\nSu porcentaje de aciertos es de "+indiceacierto+"%.\n\n"+mensaje+". ";
+}
